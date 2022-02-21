be08544f00ce3cb89e45788259c2c6ed6689957e
DESKTOP-TVPJ660\user <sekn1997@naver.com>
2022-02-21 11:43:44 +0900

    validating Block Structure

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,13 @@ class Block {
         data: string
     ): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
+    static validateStructure = (aBlock: Block): boolean =>
+        typeof aBlock.index === "number" &&
+        typeof aBlock.hash === "string" &&
+        typeof aBlock.previousHash === "string" &&
+        typeof aBlock.data === "string" &&
+        typeof aBlock.timestamp === "number"
+
     constructor (
         index: number,
         hash: string,
@@ -49,6 +56,20 @@ const createNewBlock = (data: string): Block => {
     return newBlock;
 }
 
-console.log(createNewBlock("hello"), createNewBlock("bye bye"));
+const getHashforBlock = (aBlock: Block): string => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
+
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+    if (!Block.validateStructure(candidateBlock)) {
+        return false;
+    } else if (previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    } else if (previousBlock.hash !== candidateBlock.previousHash) {
+        return false;
+    } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+        return false;
+    } else {
+        return true;
+    }
+}
 
 export { };
